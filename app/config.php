472776cfad5eb23fedecc83c6d9025af019237a7
khanh779-9 <khanh779-9@users.noreply.github.com>
2026-01-05 21:03:42 +0700

    d

--- app/config.php
+++ app/config.php
@@ -1,12 +1,12 @@
 <?php
 return [
-    'db' => [
-        'host' => '127.0.0.1',
-        'dbname' => 'db_nhasach',
-        'user' => 'root',
-        'pass' => '',
-        'charset' => 'utf8mb4',
-    ],
+    // 'db' => [
+    //     'host' => '127.0.0.1',
+    //     'dbname' => 'db_nhasach',
+    //     'user' => 'root',
+    //     'pass' => '',
+    //     'charset' => 'utf8mb4',
+    // ],
     // Production example (place credentials in env vars / secrets manager, not in source)
    // 'db' => [
     //     'host' => 'sql309.infinityfree.com',
@@ -15,13 +15,13 @@ return [
     //     'pass' => 'VX8fKDvYtSa',
     //     'charset' => 'utf8mb4',
     // ],
-    //  'db' => [
-    //     'host' => 'ballast.proxy.rlwy.net:46056',
-    //     'dbname' => 'db_nhasach',
-    //     'user' => 'root',
-    //     'pass' => 'EAWCmeStFTZmgsBTBSgZuFWeRyaiZnfL',
-    //     'charset' => 'utf8mb4',
-    // ],
+     'db' => [
+        'host' => 'ballast.proxy.rlwy.net:46056',
+        'dbname' => 'db_nhasach',
+        'user' => 'root',
+        'pass' => 'EAWCmeStFTZmgsBTBSgZuFWeRyaiZnfL',
+        'charset' => 'utf8mb4',
+    ],
     'base_url' => '/', 
     'api_key' => null, 
     'api_rate_limit' => [ 
